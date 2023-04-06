ee9b7ec6f244d1b3d211c09dbe50339037f269be
jadon paulse <jadonpaulse@gmail.com>
2023-04-06 15:45:18 +0200

    Added New input system for movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,28 +8,61 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
 
-    private float Move;
-
-    public float speed;
-    public float jump;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
 
     void Update()
     {
-        Move = Input.GetAxisRaw("Horizontal");
-
-        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump"))
+        if (!isFacingRight && horizontal > 0f)
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+            Flip();
         }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
     }
 
 }
